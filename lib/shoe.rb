88158469578187a4f16278cc20d1c20e12320df2
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-21 16:33:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,21 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
   def initialize(brand)
     @brand = brand
   end
-
+  
+  BRANDS = []
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+  
+  def brand=(brand)
+    BRANDS << @brand
+    BRANDS.uniq
   end
 
 end
